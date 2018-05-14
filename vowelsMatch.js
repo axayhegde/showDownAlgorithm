f9f3e3c2949e4ee6ed2f0124d4b6c0a96773f006
axayhegde <akshayheg@gmail.com>
2018-05-14 15:17:27 +0530

    changed to es6 notations

--- vowelsMatch.js
+++ vowelsMatch.js
@@ -4,12 +4,12 @@ and regular expression in JS
  */
 
 function vowelMatch(testString){
-    var count = 0;
-    var found;
-    var testStringArr = testString.split('');
+    let count = 0;
+    let found;
+    let testStringArr = testString.split('');
 
-    for(var i = 0 ; i < testStringArr.length; i++){
-        var str = testStringArr[i];
+    for(let i = 0 ; i < testStringArr.length; i++){
+        let str = testStringArr[i];
 
         found = str.match(/[aeiou]/gmi);
         if(found){
@@ -19,5 +19,5 @@ function vowelMatch(testString){
     console.log('The number of vowels in the given string are ' + count);
 
 }
-var testString = 'Hello this is going';
+let testString = 'Hello this is going';
 vowelMatch(testString);
