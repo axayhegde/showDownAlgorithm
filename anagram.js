59c19af02e92f1bdf55e24272c1ae51b8d350b5b
axayhegde <akshayheg@gmail.com>
2018-05-14 09:21:12 +0530

    improved to handle spaces and case differences

--- anagram.js
+++ anagram.js
@@ -3,24 +3,25 @@
 
 function anagram(myStr, myStr2) {
     var flag = true;
-    var myStrArr = myStr.split('');
-    var myStr2Arr = myStr2.split('');
+
+    var myStrArr = myStr.replace(/ /g,'').split('');
+    var myStr2Arr = myStr2.replace(/ /g,'').split('');
     myStrArr.sort();
     myStr2Arr.sort();
 
     for (var i = 0; i < myStr2Arr.length; i++) {
-        if (myStrArr[i] != myStr2Arr[i]) {
+        if (myStrArr[i].toLowerCase() != myStr2Arr[i].toLowerCase()) {
             flag = false;
         }
     }
     if (flag) {
-        console.log('The Strings ' + myStr + ' and ' + myStr2 + ' are Anagrams');
+        console.log('The Strings "' + myStr + '" and "' + myStr2 + '" are Anagrams');
     } else {
-        console.log('The Strings ' + myStr + ' and ' + myStr2 + ' are not Anagrams');
+        console.log('The Strings "' + myStr + '" and "' + myStr2 + '" are not Anagrams');
     }
 }
 
-var myStr = "akshay";
-var myStr2 = "askhsy";
+var myStr = "akshay is the best";
+var myStr2 = "Askhay si eht sebt";
 
 anagram(myStr, myStr2);
